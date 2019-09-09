033a9b8f5ab9b1b43d2ce6c828f23984bad388ed
JacintoOrtiz15745 <andresjacinto_araujo@ucol.mx>
2019-09-09 13:36:10 -0500

    All the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   get 'welcome/index'
+
   devise_for :users
+  resources :tasks
+  resources :lists do
+    resources :tasks
+  end
+  
+  root 'welcome#index'
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190904180014) do
+ActiveRecord::Schema.define(version: 20190908193512) do
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "task"
+    t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_tasks_on_list_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
